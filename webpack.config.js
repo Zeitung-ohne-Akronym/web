7564c7c72d70c3e22f12ac3ecc412530ddb9ee7c
nleanba <noam@factsmission.com>
2019-10-03 20:47:44 +0200

    scss included

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,37 @@ const path = require('path');
 
 
 module.exports = {
-    entry: './src/javascript/index.js',
+    entry: ['./src/javascript/index.js', './src/styles/index.scss'],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }, {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'bundle.css',
+                        },
+                    },
+                    { loader: 'extract-loader' },
+                    { loader: 'css-loader' },
+                    { loader: 'sass-loader' },
+                ]
+            }
+        ]
     },
 
     // Default mode for Webpack is production.
