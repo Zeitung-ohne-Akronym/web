e4a53d4769fbeb6ee09477438cab25dffd164477
nleanba <noam@factsmission.com>
2019-10-03 20:31:54 +0200

    added basic framework

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+
+module.exports = {
+    entry: './src/javascript/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+
+    // Default mode for Webpack is production.
+    // Depending on mode Webpack will apply different things
+    // on final bundle. For now we don't need production's JavaScript 
+    // minifying and other thing so let's set mode to development
+    mode: 'development'
+};
