45472ecf082e4d7c45b54cc299bcfdc13717d055
nleanba <noam@factsmission.com>
2019-10-03 22:18:06 +0200

    added pages

---
+++ src/javascript/material.js
@@ -0,0 +1,5 @@
+import {MDCTopAppBar} from '@material/top-app-bar';
+
+// Instantiation
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+if (topAppBarElement) new MDCTopAppBar(topAppBarElement);
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
     entry: ['./src/javascript/index.js', './src/styles/index.scss'],
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
         filename: 'bundle.js'
     },
     module: {
@@ -29,7 +29,15 @@ module.exports = {
                     },
                     { loader: 'extract-loader' },
                     { loader: 'css-loader' },
-                    { loader: 'sass-loader' },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sassOptions: {
+                                includePaths: [path.join(__dirname, 'node_modules'), path.join(__dirname, 'node_modules/@material/*')]
+                                    .reduce((a, c) => a.concat(c), [])
+                            }
+                        }
+                    },
                 ]
             }
         ]
@@ -39,5 +47,6 @@ module.exports = {
     // Depending on mode Webpack will apply different things
     // on final bundle. For now we don't need production's JavaScript 
     // minifying and other thing so let's set mode to development
-    mode: 'development'
+    mode: 'development',
+    devtool: 'source-map'
 };
