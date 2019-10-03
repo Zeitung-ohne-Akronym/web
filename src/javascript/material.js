3200a1bd6a505c895d93889c05c1cfe5ea559c9f
nleanba <noam@factsmission.com>
2019-10-03 22:57:17 +0200

    vorarbeiten

--- src/javascript/material.js
+++ src/javascript/material.js
@@ -1,5 +1,9 @@
 import {MDCTopAppBar} from '@material/top-app-bar';
+import {MDCRipple} from '@material/ripple';
 
-// Instantiation
+const selector = '.mdc-button, .mdc-card__primary-action';
+[].map.call(document.querySelectorAll(selector), (el) => new MDCRipple(el));
+const iconButtonRipple = new MDCRipple(document.querySelector('.mdc-icon-button'));
+iconButtonRipple.unbounded = true;
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 if (topAppBarElement) new MDCTopAppBar(topAppBarElement);
